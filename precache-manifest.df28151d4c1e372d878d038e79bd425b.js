e65efcdcbf67ba6d7a6309aa8b4c63853195a8b9
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2019-10-02 19:50:40 +0700

    Updates

---
+++ precache-manifest.df28151d4c1e372d878d038e79bd425b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3554c24a3ed1f65659f077ce67f6a36",
+    "url": "/Game_Caro/index.html"
+  },
+  {
+    "revision": "50d43ae251d32c323f30",
+    "url": "/Game_Caro/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4f3d3e6da46b14952057",
+    "url": "/Game_Caro/static/js/2.527329ef.chunk.js"
+  },
+  {
+    "revision": "50d43ae251d32c323f30",
+    "url": "/Game_Caro/static/js/main.213bd30a.chunk.js"
+  },
+  {
+    "revision": "d22307f76a02fb0ac27c",
+    "url": "/Game_Caro/static/js/runtime~main.ec8746d1.js"
+  }
+]);
